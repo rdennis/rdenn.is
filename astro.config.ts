a9c950ddd0a1987bac6cbfff0cb777d7805a5f4b
Robert Dennis <rdennis@rdenn.is>
2026-06-19 21:11:27 -0500

    Added github pages deploy

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://rdennis.github.io/rdenn.is/',
+    base: '/rdenn.is',
+});
